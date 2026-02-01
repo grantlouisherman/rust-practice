3ccc5c5e4414e22d54b49623cd787d6e0dfd9dac
grantlouisherman <grantlouisherman041@gmail.com>
2026-02-01 17:09:51 -0500

    input bug

--- rpg_shop/src/main.rs
+++ rpg_shop/src/main.rs
@@ -1,3 +1,4 @@
+use std::io;
 
 #[derive(Debug, Clone)]
 struct Item {
@@ -49,29 +50,66 @@ impl Player {
     }
 }
 
-struct Shop {
-    inventory: Vec<Item>
-}
+// struct Shop {
+//     inventory: Vec<Item>
+// }
 
 
-impl Shop {
-    fn bargin(item_id: String) -> f32 {
-        /*
-            impl_bargin(): -> Roll random 1 ups the price by x2
-            Roll 2-4 nothing
-            Roll 5 10% off
-            Rolle 6 15% off
-        */
-        1.0
-    }
-}
+// impl Shop {
+//     fn bargin(item_id: String) -> f32 {
+//         /*
+//             impl_bargin(): -> Roll random 1 ups the price by x2
+//             Roll 2-4 nothing
+//             Roll 5 10% off
+//             Rolle 6 15% off
+//         */
+//         1.0
+//     }
+// }
 
 
 fn main() {
+    // Item Bank
     let bank = ItemsBank::new().bank;
+
+    // Player setup 
     let items_for_player = &bank[0..5];
-    print!("LEN --- >{}", items_for_player.len());
     let items_vec: Vec<Item> = items_for_player.to_vec();
     let player = Player::spawn(items_vec, String::from("Test Player"));
-    print!("{:?}", player);
+
+    /*
+        commands:
+        - check shop(cs) -> display all items in the shop as a table
+        - query shop(qs) [item] -> search for an item
+        - sell [item] (s) -> sell and item
+        - check inventiory -> prints out what you have
+        - exit - exit program
+
+    */
+    let mut input = String::new();
+    let help_text = String::from("
+    commands: \n
+    - check shop(cs) -> display all items in the shop as a table \n
+    - query shop(qs) [item] -> search for an item \n
+    - sell [item] (s) -> sell and item \n
+    - check inventiory -> prints out what you have \n
+    - exit - exit program \n
+    - help
+    ");
+    print!("{}\n", help_text);
+   loop {
+        io::stdin().read_line(&mut input).expect("Failed");
+        let ci = input.trim();
+        //print!("{}\n", ci);
+        if ci == "exit"  {
+            break;
+        }
+        if ci == "help" {
+            print!("{}", help_text);
+            continue;
+        }
+        print!("Command not understood: {}\n", ci);
+    }
+
+    print!("\nGame Over\n");
 }
