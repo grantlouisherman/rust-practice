691b4a4325318d15d769d0a48d827b3f8a7594c6
grantlouisherman <grantlouisherman041@gmail.com>
2026-05-23 22:55:37 -0400

    more stuff

---
+++ minigrep/src/lib.rs
@@ -0,0 +1,3 @@
+pub fn search<'a>(query: &str, content: &'a str) -> Vec<&'a str> {
+    unimplemented!();
+}
--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,16 +1,41 @@
 use std::env;
 use std::fs;
-
+use std::process;
+use std::error::Error;
+use minigrep::search;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let (query, file_path) = parse_args(&args);
-    let contents = fs::read_to_string(file_path).expect("Read File Error");
-    println!("{contents}");
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem parsing error {err}");
+        process::exit(1);
+    });
+    if let Err(e) = run(config) {
+        println!("Application error: {e}");
+        process::exit(1);
+    }
 }
 
-fn parse_args(args:&[String]) -> (&str, &str) {
-    let query = &args[1];
-    let file_path = &args[2];
-    (query, file_path)
+fn run(config:Config) -> Result<(), Box<dyn Error>> {
+    let content = fs::read_to_string(config.file_path)?;
+    for line in search(&config.query, &content) {
+        println!("{line}");
+    }
+    Ok(())
 }
+struct Config {
+    pub query: String,
+    pub file_path: String
+}
+
+impl Config {
+    fn build(args:&[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("Too few arguments");
+        }
+        let query = String::from(&args[1]);
+        let file_path = String::from(&args[2]);
+        Ok(Config{query, file_path})
+    }
+}
+
