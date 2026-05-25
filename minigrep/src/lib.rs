e05727fd8cd67f1247ff61a0a80935e50183d295
grantlouisherman <grantlouisherman041@gmail.com>
2026-05-24 23:00:54 -0400

    completeld minigrep tutorial

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -1,3 +1,34 @@
+use std::env;
+
+
 pub fn search<'a>(query: &str, content: &'a str) -> Vec<&'a str> {
-    unimplemented!();
+    let mut found_lines = vec![];
+    let ignore_case =. env::var("IGNORE_CASE").is_ok();
+    
+    let query = query.to_lowercase();
+    for line in content.lines() {
+        if line.to_lowercase().contains(&query) {
+            found_lines.push(line);
+        }
+    }
+    found_lines
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let content = "\nRust:\nsafe, fast, productive.\nPick three.";
+        assert_eq!(vec!["safe, fast, productive."], search(query, content));
+    }
+    #[test]
+    fn case_insensitive() {
+        let query = "rUsT";
+        let content = "\nRust:\nsafe, fast, productive.\nPick three.\nTrust me.";
+        assert_eq!(vec!["Rust:", "Trust me."], search(query, content));
+    }
+
 }
